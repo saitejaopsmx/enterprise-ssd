080274db65c0aad3c8ce25020267e403552fc440
Ghanithan Subramani <ghanithan.subramani@gmail.com>
2025-04-17 17:00:13 +0530

    mod

--- vm-install/image-setup/clean-before-build.sh
+++ vm-install/image-setup/clean-before-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo rm bundle.sh extract-images-list.sh rendered.yaml image-list.txt pull-images.sh
+sudo rm bundle.sh extract-images-list.sh rendered.yaml image-list.txt pull-images.sh version.env
 sudo rm -- $0
 
 sleep 1
